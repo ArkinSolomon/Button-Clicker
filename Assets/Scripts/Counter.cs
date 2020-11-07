51e3a5831617cced65a497eef6aa0ebd63fb0e9b
ArkinSolomon <arkinsolomon@gmail.com>
2020-11-07 11:10:24 -0800

    Done I think

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+public class Counter : MonoBehaviour
+{
+    int timesPressed = 0;
+    public TMP_Text counter;
+    void Start()
+    {       
+        if (PlayerPrefs.HasKey("pressed"))
+            timesPressed = PlayerPrefs.GetInt("pressed");
+        counter.text = timesPressed.ToString();
+    }
+    public void Press()
+    {
+        timesPressed++;
+        PlayerPrefs.SetInt("pressed", timesPressed);
+        counter.text = timesPressed.ToString();
+    }
+}
